3cd92553615165b63854e4ddcb0c3710d7f61ceb
lydiasebaziga <lydiase.baziga@gmail.com>
2026-09-23 09:15:26 -0700

    javascrip fix

--- js/form.js
+++ js/form.js
@@ -6,51 +6,61 @@ let resultMessage = document.querySelector(".result-message");
 let resultNote = document.querySelector(".result-note");
 let resultImage = document.querySelector(".result-image");
 
-// Variables for our output
-let output = "";
-let imgAlt = "";
-let imgSrc = "";
 
-// addevenlisten for form submit
-form.addEventListener("submit", function(event) {
-event.preventDefault();
-let formData = new FormData(form);
-let data = Object.fromEntries(formData);
-	console.log(data);
-	
-let mood = data.mood;
-let note = data.feel;
+// Function to check the mood
+function checkMood(mood) {
 
-console.log(mood);
-console.log(note);
+    if (mood === "happy") {
 
+        return {
+            message: "You are feeling happy! Keep smiling!",
+            image: "https://cdn-icons-png.flaticon.com/512/742/742751.png",
+            alt: "Happy face"
+        };
 
-// if else statement 
-if (mood === "happy") {
+    } else if (mood === "sad") {
 
-    output = "You are feeling happy! Keep smiling!";
-    imgAlt = "Happy face emoji";
-    imgSrc = "https://cdn-icons-png.flaticon.com/512/742/742751.png";
+        return {
+            message: "It's okay to feel sad. Take some time for yourself.",
+            image: "https://cdn-icons-png.flaticon.com/512/742/742752.png",
+            alt: "Sad face"
+        };
 
-} else if (mood === "sad") {
+    } else if (mood === "calm") {
 
-    output = "It's okay to feel sad. Take some time for yourself.";
-    imgAlt = "Sad face emoji";
-    imgSrc = "https://cdn-icons-png.flaticon.com/512/742/742752.png";
-
-} else if (mood === "calm") {
-
-    output = "You are feeling calm. Enjoy this peaceful moment!";
-    imgAlt = "Calm emoji";
-    imgSrc = "https://cdn-icons-png.flaticon.com/512/5786/5786952.png";
+        return {
+            message: "You are feeling calm. Enjoy this peaceful moment!",
+            image: "https://cdn-icons-png.flaticon.com/512/5786/5786952.png",
+            alt: "Calm face"
+        };
+    }
 }
 
 
-// Update the HTML
-resultMessage.textContent = output;
-resultNote.textContent = note;
-resultImage.src = imgSrc;
-resultImage.alt = imgAlt;
+// Function to submit the form
+function submitForm(event) {
+
+    event.preventDefault();
+
+    // Get the form data
+    let formData = new FormData(form);
+    let data = Object.fromEntries(formData);
+
+    // Get the mood and note
+    let mood = data.mood;
+    let note = data.feel;
+
+    // Check the mood
+    let result = checkMood(mood);
+
+    // Show the results
+    resultMessage.textContent = result.message;
+    resultNote.textContent = note;
+    resultImage.src = result.image;
+    resultImage.alt = result.alt;
+}
 
 
-});
+// Listen for the form submission
+form.addEventListener("submit", submitForm);
+
